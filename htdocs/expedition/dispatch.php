6c03485e62dca7011bc110122408e74bd5981b77
VESSILLER <lvessiller@open-dsi.fr>
2025-03-14 15:42:24 +0100

    Backport hidden warehouse in dispatch when stockable product was integrated on develop (PR 32326)

--- htdocs/expedition/dispatch.php
+++ htdocs/expedition/dispatch.php
@@ -1064,6 +1064,10 @@ if ($object->id > 0 || !empty($object->ref)) {
 											$langs->load("errors");
 											print $langs->trans("ErrorNoWarehouseDefined");
 										}
+									} else {
+										// on force l'entrepot pour passer le test d'ajout de ligne dans expedition.class.php
+										print '<input id="entrepot'.$suffix.'" name="entrepot'.$suffix.'" type="hidden" value="'.$objd->fk_warehouse.'">';
+										print img_warning().' '.$langs->trans('StockDisabled');
 									}
 									print "</td>\n";
 
